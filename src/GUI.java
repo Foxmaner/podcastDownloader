cc94a20cfa74957afef6a992f9973df194d575aa
Eskil Brännerud <eskilb@kth.se>
2021-04-07 00:23:22 +0200

    Lade till GUI för 3/4 fönster

--- src/GUI.java
+++ src/GUI.java
@@ -2,25 +2,66 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class GUI {
 	
 	public GUI() {
+		
+		//Skapa frame och layout
 		JFrame frame = new JFrame();
 		frame.setLayout(new GridLayout(4,1));
 		
 		
 		
+		//Skapa panel för att skriva in länkar
 		JPanel linkSelectPanel = new JPanel();
+		linkSelectPanel.setLayout(new GridLayout(3,1));
+		
+		JLabel linkInstructionLabel = new JLabel("Skriv in länken till RSS filen");
+		JTextField inputLinkField = new JTextField();
+		JButton fetchProgramButton = new JButton("Hämta program (Ej ladda ner)");
+		
+		
+		linkSelectPanel.add(linkInstructionLabel);	
+		linkSelectPanel.add(inputLinkField);	
+		linkSelectPanel.add(fetchProgramButton);
+		
+		
+		
+		//Skapa panel för att se och välja program
 		JPanel programSelectPanel = new JPanel();
+		programSelectPanel.setLayout(new GridLayout(1,2));
+		
+		JTextArea fetchedProgramsTextArea = new JTextArea();
+		JTextArea selectedProgramsTextArea = new JTextArea();
+		
+		programSelectPanel.add(fetchedProgramsTextArea);
+		programSelectPanel.add(selectedProgramsTextArea);
+		
+		//Skapa panel för att välja nerladdningsplats
 		JPanel downloadSelectPanel = new JPanel();
+		
+		downloadSelectPanel.setLayout(new GridLayout(3,1));
+		
+		JLabel downloadLocationInstructionLabel = new JLabel("Välj plats för nerladdning");
+		JTextField downloadLocationkField = new JTextField();
+		JButton downloadProgramButton = new JButton("Ladda ner valda program");
+		
+		
+		downloadSelectPanel.add(downloadLocationInstructionLabel);	
+		downloadSelectPanel.add(downloadLocationkField);	
+		downloadSelectPanel.add(downloadProgramButton);
+		
+		//Skapa panel för konsol/status
 		JPanel consolePanel = new JPanel();
 		
-		JButton fetchProgramButton = new JButton("Hämta program");
-		
-		
+
 		
 		frame.add(linkSelectPanel);
 		frame.add(programSelectPanel);
