ff9e44a39e759b5cae537de8806e7f920ec27ace
Eskil Brännerud <eskilb@kth.se>
2021-04-07 10:24:30 +0200

    Deklarerade samtliga GUI

--- src/GUI.java
+++ src/GUI.java
@@ -9,24 +9,49 @@ import javax.swing.JTextField;
 
 public class GUI {
 	
+	//Deklarera frame
+	JFrame frame;
+	
+	
+	//Deklarera linkSelectPanel
+	JPanel linkSelectPanel;
+	JLabel linkInstructionLabel; 
+	JTextField inputLinkField;
+	JButton fetchProgramButton;
+	
+	//Deklarera programSelectPanel
+	JPanel programSelectPanel;
+	JTextArea fetchedProgramsTextArea;
+	JTextArea selectedProgramsTextArea;
+	
+	//Deklarera downloadSelectionPanel
+	JPanel downloadSelectPanel;
+	JLabel downloadLocationInstructionLabel;
+	JTextField downloadLocationkField;
+	JButton downloadProgramButton;
+	
+	//Deklarera konsolePanel
+	JPanel consolePanel;
+	JTextArea consoleStatusTextArea;
+	
+	
 	public GUI() {
-		
 		//
 		//Skapa frame och layout
 		//
-		JFrame frame = new JFrame();
+		frame = new JFrame();
 		frame.setLayout(new GridLayout(4,1));
 		
 		
 		//
 		//Skapa panel för att skriva in länkar
 		//
-		JPanel linkSelectPanel = new JPanel();
+		linkSelectPanel = new JPanel();
 		linkSelectPanel.setLayout(new GridLayout(3,1));
 		
-		JLabel linkInstructionLabel = new JLabel("Skriv in länken till RSS filen");
-		JTextField inputLinkField = new JTextField();
-		JButton fetchProgramButton = new JButton("Hämta program (Ej ladda ner)");
+		linkInstructionLabel = new JLabel("Skriv in länken till RSS filen");
+		inputLinkField = new JTextField();
+		fetchProgramButton = new JButton("Hämta program (Ej ladda ner)");
 		
 		
 		linkSelectPanel.add(linkInstructionLabel);	
@@ -37,11 +62,11 @@ public class GUI {
 		//
 		//Skapa panel för att se och välja program
 		//
-		JPanel programSelectPanel = new JPanel();
+		programSelectPanel = new JPanel();
 		programSelectPanel.setLayout(new GridLayout(1,2));
 		
-		JTextArea fetchedProgramsTextArea = new JTextArea();
-		JTextArea selectedProgramsTextArea = new JTextArea();
+		fetchedProgramsTextArea = new JTextArea();
+		selectedProgramsTextArea = new JTextArea();
 		
 		programSelectPanel.add(fetchedProgramsTextArea);
 		programSelectPanel.add(selectedProgramsTextArea);
@@ -49,12 +74,12 @@ public class GUI {
 		//
 		//Skapa panel för att välja nerladdningsplats
 		//
-		JPanel downloadSelectPanel = new JPanel();
+		downloadSelectPanel = new JPanel();
 		downloadSelectPanel.setLayout(new GridLayout(3,1));
 		
-		JLabel downloadLocationInstructionLabel = new JLabel("Välj plats för nerladdning");
-		JTextField downloadLocationkField = new JTextField();
-		JButton downloadProgramButton = new JButton("Ladda ner valda program");
+		downloadLocationInstructionLabel = new JLabel("Välj plats för nerladdning");
+		downloadLocationkField = new JTextField();
+		downloadProgramButton = new JButton("Ladda ner valda program");
 		
 		
 		downloadSelectPanel.add(downloadLocationInstructionLabel);	
@@ -64,10 +89,10 @@ public class GUI {
 		//
 		//Skapa panel för konsol/status
 		//
-		JPanel consolePanel = new JPanel();
+		consolePanel = new JPanel();
 		consolePanel.setLayout(new GridLayout(1,1));
 		
-		JTextArea consoleStatusTextArea = new JTextArea("DDD");
+		consoleStatusTextArea = new JTextArea("DDD");
 		
 		consolePanel.add(consoleStatusTextArea);
 		
